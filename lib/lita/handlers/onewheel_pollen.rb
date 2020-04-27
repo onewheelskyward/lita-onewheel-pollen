5e760968dcb5c52439a8178238fe39c5e1d23bc4
Andrew Kreps <andrew.kreps@gmail.com>
2020-04-27 10:22:33 -0700

    Fixed!  Thanks @donpdonp

--- lib/lita/handlers/onewheel_pollen.rb
+++ lib/lita/handlers/onewheel_pollen.rb
@@ -16,21 +16,26 @@ module Lita
         end
         cookie_str.sub! /; $/, ''
 
-        headers = {'Accept': 'application/json, text/plain, */*',
+        headers = {
+            #'Accept': 'application/json, text/plain, */*',
                    # 'Accept-Encoding': 'gzip, deflate, br',
                    # 'Accept-Language': 'en-US,en;q=0.9,it;q=0.8',
                    # 'Connection': 'keep-alive',
-                   'Cookie': cookie_str,
-                   'Host': 'www.pollen.com',
-                   'Origin': 'https://www.pollen.com',
+                   # 'Cookie': cookie_str,
+                   # 'Host': 'www.pollen.com',
+                   # 'Origin': 'https://www.pollen.com',
                    'Referer': 'https://www.pollen.com/forecast/extended/pollen/97206',
                    # 'Sec-Fetch-Dest': 'empty',
                    # 'Sec-Fetch-Mode': 'cors',
                    # 'Sec-Fetch-Site': 'same-origin',
-                   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'
+                   'User-Agent': 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0'
+            #'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'
         }
         puts headers
-        resp2 = RestClient.get "https://www.pollen.com/api/forecast/extended/pollen/97206", {headers: headers}
+        resp2 = RestClient::Request.execute(
+                  method: :get,
+                  url: "https://www.pollen.com/api/forecast/extended/pollen/97206",
+                  headers: headers)
         puts resp2
       end
 
